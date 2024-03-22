3ba48a4c92844d8479f0fc72ba6e33bc972cd27a
Rodrigo1619 <00067920@uca.edu.sv>
2024-03-21 21:23:04 -0600

    culebra

---
+++ ola.py
@@ -0,0 +1,12 @@
+import pyautogui as pt
+import time
+
+limit = input("Cantidad de mensajes a mandar: ")
+message = input("Mensaje: ")
+i = 0
+time.sleep(3)
+
+while i < int(limit):
+        pt.typewrite(message)
+        pt.press("enter")
+        i += 1
